fb70b722d0a912256600cb0577d4c6adce0b445e
Akram Khan <ruletheworld2000@gmail.com>
2017-07-06 16:34:56 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+
+
+        $('.carousel.carousel-slider').carousel({});
+
+}); // end of document ready
+})(jQuery); // end of jQuery name space
